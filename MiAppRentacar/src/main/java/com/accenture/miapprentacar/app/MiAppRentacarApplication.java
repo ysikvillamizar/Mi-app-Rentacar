d5fa292aca71c48fede9016953080d0de9ad2f29
JESSICA VILLAMIZAR SUAZA <jessicavillamizar87135@correo.itm.edu.co>
2019-09-04 14:20:44 -0500

    mi commit master

---
+++ MiAppRentacar/src/main/java/com/accenture/miapprentacar/app/MiAppRentacarApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.miapprentacar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiAppRentacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiAppRentacarApplication.class, args);
+	}
+
+}
